da8610846fc92b2655d372131300d39f3a842167
Joe Clay <27cupsofcoffee@gmail.com>
2019-07-09 15:48:28 +0100

    Fix some references to the old await! syntax (#21)

--- examples/01_04_async_await_primer/src/lib.rs
+++ examples/01_04_async_await_primer/src/lib.rs
@@ -43,7 +43,7 @@ mod third {
 use super::*;
 async fn learn_and_sing() {
     // Wait until the song has been learned before singing it.
-    // We use `await!` here rather than `block_on` to prevent blocking the
+    // We use `.await` here rather than `block_on` to prevent blocking the
     // thread, which makes it possible to `dance` at the same time.
     let song = learn_song().await;
     sing_song(song).await;
@@ -53,7 +53,7 @@ async fn async_main() {
     let f1 = learn_and_sing();
     let f2 = dance();
 
-    // `join!` is like `await!` but can wait for multiple futures concurrently.
+    // `join!` is like `.await` but can wait for multiple futures concurrently.
     // If we're temporarily blocked in the `learn_and_sing` future, the `dance`
     // future will take over the current thread. If `dance` becomes blocked,
     // `learn_and_sing` can take back over. If both futures are blocked, then
