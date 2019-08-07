40a5ce99c152749ad08b003db2ed9bbeaf370fe0
Mr.Panda <xivistudios@gmail.com>
2019-08-07 17:08:04 +0800

    Update the comments in the 1.1 sample code to Chinese.

--- examples/01_02_why_async/src/lib.rs
+++ examples/01_02_why_async/src/lib.rs
@@ -16,11 +16,11 @@ fn download(_url: &str) {
 #[test]
 // ANCHOR: get_two_sites
 fn get_two_sites() {
-    // Spawn two threads to do work.
+    // 生成两个线程来下载网页.
     let thread_one = thread::spawn(|| download("https:://www.foo.com"));
     let thread_two = thread::spawn(|| download("https:://www.bar.com"));
 
-    // Wait for both threads to complete.
+    // 等待两个线程运行下载完成.
     thread_one.join().expect("thread one panicked");
     thread_two.join().expect("thread two panicked");
 }
@@ -32,12 +32,11 @@ async fn download_async(_url: &str) {
 
 // ANCHOR: get_two_sites_async
 async fn get_two_sites_async() {
-    // Create a two different "futures" which, when run to completion,
-    // will asynchronously download the webpages.
+    // 创建两个不同的 "futures", 当创建完成之后将异步下载网页.
     let future_one = download_async("https:://www.foo.com");
     let future_two = download_async("https:://www.bar.com");
 
-    // Run both futures to completion at the same time.
+    // 同时运行两个 "futures" 直到完成.
     join!(future_one, future_two);
 }
 // ANCHOR_END: get_two_sites_async
